e23edc7e983d1cc7a25e3f7b991ea6629fe0c9a2
WorldInMine <360641611@qq.com>
2017-09-16 23:03:51 +0800

    Initial commit

---
+++ src/main/java/com/wzh/PersonalwebApplication.java
@@ -0,0 +1,12 @@
+package com.wzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalwebApplication.class, args);
+	}
+}
