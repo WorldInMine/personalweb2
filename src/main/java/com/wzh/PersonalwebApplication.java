9d305f971a4ec7e967df301061c35e3f66d04042
WorldInMine <360641611@qq.com>
2017-09-17 16:33:41 +0800

    two

--- src/main/java/com/wzh/PersonalwebApplication.java
+++ src/main/java/com/wzh/PersonalwebApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PersonalwebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PersonalwebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalwebApplication.class, args);
+    }
 }
