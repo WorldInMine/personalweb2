b64e0ccf92d6a4b3103e7aeb961a108354863aeb
WorldInMine <360641611@qq.com>
2017-09-17 22:38:47 +0800

    3

--- src/main/java/com/wzh/controller/TestController.java
+++ src/main/java/com/wzh/controller/TestController.java
@@ -1,7 +1,23 @@
 package com.wzh.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+
+@Controller
 public class TestController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
+
+    @RequestMapping("/")
+    public String index(){
+        ModelAndView modelAndView=new ModelAndView("/index");
+        return "index";
+    }
 }
