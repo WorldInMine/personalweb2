e1361a5126a3959fe0d569459256cb91c9a46148
WorldInMine <360641611@qq.com>
2017-10-10 13:11:17 +0800

    a
    index yeimian

--- src/main/java/com/wzh/controller/TestController.java
+++ src/main/java/com/wzh/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.wzh.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
@@ -19,5 +22,19 @@ public class TestController {
     public String index(){
         ModelAndView modelAndView=new ModelAndView("/index");
         return "index";
+    }
+
+    /**
+     * 捕获类内所有的异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public ModelAndView exceptionHandelByThymeleaf(Exception ex,HttpServletRequest req){
+        ModelAndView mav=new ModelAndView();
+        mav.setViewName("error");
+        mav.addObject("exception", ex);
+        mav.addObject("url", req.getRequestURL());
+        return mav;
     }
 }
